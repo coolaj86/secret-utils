62223b5b1160365c2683ca28d375788fd6794476
AJ ONeal <awesome@coolaj86.com>
2014-10-13 15:05:47 -0700

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,110 @@ secret-utils
 ============
 
 Common `crypto` snippets (generate random bytes, salt, hash password, etc)
+
+You should use the Node.js modules for `bcrypt` or `scrypt`,
+but if you'd rather have pure Node.js JavaScript without compiled modules,
+well, here you go.
+
+```bash
+npm install secret-utils
+```
+
+```javascript
+var secretutils = require('secret-utils')
+  ;
+
+secretutils.url64(92);
+// NOTE: 96 bytes becomes a 128-char string
+```
+
+API
+===
+
+  * createShadow
+  * testSecret
+  * url64
+  * random
+  * int
+  * hashsum
+  * md5sum
+  * alphanum
+
+#### `createShadow(secret[, hashtype[, salt ]])`
+
+Given a secret (password, passphrase, etc), returns a shadow, hashtype, and salt.
+
+`hashtype` defaults to `md5` (despite collisions being possible, I'd hardly say that md5 is "broken")
+
+`salt` defaults to `url64(32)`
+
+```javascript
+{ salt: '8B3Mfmrt5kXVg9nfIFHxUY9F4ii3bIJGKr7uoQHsTgc='
+, shadow: 'f486460617d1501b51e6807c5e4f2ded'
+, hashtype: 'md5'
+}
+```
+
+#### `testSecret(salt, secret, shadow[, hashtype ])`
+
+Given a salt, secret, shadow (and hashtype), determine if the secret matches the shadow.
+
+`hashtype` defaults to `md5`
+
+returns `true` or `false`
+
+#### `genSalt(len)`
+
+Creates some salt like so: `return url64(len || 32);`
+
+#### `url64(len)`
+
+Creates a url-safe base64 string with a given entropy
+
+```javascript
+crypto.randomBytes(len || 32)
+  .toString('base64')
+ .replace(/\+/g, '-')     // Convert '+' to '-'
+ .replace(/\//g, '_')     // Convert '/' to '_'
+ .replace(/=+$/, '')      // Remove ending '='
+ ;
+```
+
+#### `random(len[, encoding])`
+
+Generate a securely random `Buffer` with `len` bytes of entropy, optionally encoded as a string.
+
+#### `int(min, max)`
+
+Generate a securely random 48-bit integer.
+
+#### `hashsum(hashtype, str)`
+
+Return the hash of a given string. Useful for short strings, not for large buffers.
+
+```javascript
+return require('crypto').createHash(hashtype).update(val).digest('hex');
+```
+
+#### `md5sum(str)`
+
+Return the md5sum of a given string. Useful for short strings, not for large buffers.
+
+```javascript
+return require('crypto').createHash('md5').update(val).digest('hex');
+```
+
+#### `alphanum(len)`
+
+Return an alphanumeric (A-Za-z0-9) string (insecure, using `Math.random()`).
+
+Why?
+====
+
+Most of the `crypto` functions are built on a stream-esque API,
+but many of the common use cases for crypto involve very short strings.
+
+`md5sum(str)` is simply easier to read (and write) at-a-glance
+than `require('crypto').createHash('md5').update(val).digest('hex');`
+
+Removing a minor annoyance, that's all.
---
+++ index.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var crypto = require('crypto')
+  ;
+
+/**
+ * Return a random int, used by `utils.uid()`
+ *
+ * @param {Number} min
+ * @param {Number} max
+ * @return {Number}
+ * @api private
+ */
+function getInsecureRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Return a unique identifier with the given `len`.
+ *
+ *     utils.uid(10);
+ *     // => "FDaS435D2z"
+ *
+ * @param {Number} len
+ * @return {String}
+ * @api private
+ */
+function alphanum(len) {
+  var buf = []
+    , chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    , charlen = chars.length
+    , i
+    ;
+
+  for (i = 0; i < len; i += 1) {
+    buf.push(chars[getInsecureRandomInt(0, charlen - 1)]);
+  }
+
+  return buf.join('');
+}
+
+function int(min, max) {
+    // rand, randInt(min, max), randRange(n) [0-n)
+  var secRand = require('crypto-rand')
+    ;  
+
+  return secRand.randInt(min, max);
+}
+
+function createShadow(secret, hashtype, salt) {
+  hashtype = hashtype || 'md5';
+
+  salt = salt || crypto.randomBytes(32).toString('base64');
+
+  var hash = crypto.createHash(hashtype)
+    , shadow
+    ;
+
+  hash.update(salt);
+  hash.update(secret);
+  shadow = hash.digest('hex');
+
+  return { salt: salt, secret: shadow, shadow: shadow, type: hashtype, hashtype: hashtype };
+}
+
+function url64(len) {
+  var UrlSafeBase64 = require('urlsafe-base64')
+    ;
+
+  return UrlSafeBase64.encode(crypto.randomBytes(len || 32));
+}
+
+function random(len, encoding) {
+  if (encoding) {
+    return crypto.randomBytes(len || 32).toString(encoding);
+  } else {
+    return crypto.randomBytes(len || 32);
+  }
+}
+
+function testSecret(salt, secret, shadow, hashtype) {
+  hashtype = hashtype || 'md5';
+
+  var crypto = require('crypto')
+    , hash = crypto.createHash(hashtype)
+    ;
+
+  hash.update(salt);
+  hash.update(secret);
+
+  return shadow === hash.digest('hex');
+}
+
+function md5sum(val) {
+  return require('crypto').createHash('md5').update(val).digest('hex');
+}
+
+function hashsum(hashtype, val) {
+  return require('crypto').createHash(hashtype).update(val).digest('hex');
+}
+
+// deprecated
+module.exports.createSecretHash = createShadow;
+module.exports.testSecretHash = testSecret;
+module.exports.randomBytes = random;
+module.exports.testShadow = testSecret;
+
+// API
+module.exports.createShadow = createShadow;
+module.exports.testSecret = testSecret;
+module.exports.url64 = url64;
+module.exports.genSalt = url64;
+module.exports.random = random;
+module.exports.alphanum = alphanum;
+module.exports.int = int;
+module.exports.md5sum = md5sum;
+module.exports.hashsum = hashsum;
